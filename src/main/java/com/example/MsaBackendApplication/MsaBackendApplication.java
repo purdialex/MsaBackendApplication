09b387cf1cb7e9572d00eced1273452b6b24fe89
purdialex <purdialex67@gmail.com>
2024-11-22 16:59:29 +0200

    Initial Commit for MSA Application

---
+++ src/main/java/com/example/MsaBackendApplication/MsaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.MsaBackendApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MsaBackendApplication/MsaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MsaBackendApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
